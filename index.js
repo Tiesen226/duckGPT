cff941ebc50e33fe6d9c336f4704d37d1b9684e2
Trần Tiến <101703006+Tiesen226@users.noreply.github.com>
2023-02-28 14:26:14 +0000

    Main delay submit

--- index.js
+++ index.js
@@ -1,62 +1,94 @@
-const submitBtn = document.querySelector('.chatForm button[type="submit"]');
 const chatBox = document.querySelector('.chatBox');
 const audio = document.querySelector('audio');
+const chatMess = document.querySelector('.chatForm input[type="text"]');
+const chatForm = document.querySelector('.chatForm');
+const errorMess = document.querySelector('.errorMess');
 
-submitBtn.addEventListener('click', () => {
-    submit();
+let quackArray = [];
+let userArray = [];
+let canSubmit = true;
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleSubmit(e);
 });
 
 document.addEventListener('keydown', (e) => {
-    if (e.code == 'Enter') submit();
+    if (e.code == 'Enter') handleSubmit(e);
 });
 
 function submit() {
+    const message = chatMess.value;
 
-    let messValue = document.querySelector('.chatForm input[type="text"]').value;
     const chatItem = document.createElement('div');
-
     chatItem.classList.add('user-chat-item');
-    chatItem.innerHTML = `<img src="./user.png" alt="userIcon" id="userIcon"/>&ensp;&ensp;${messValue}`;
+    chatItem.innerHTML = `<img src="./user.png" alt="userIcon" id="userIcon"/>&ensp;&ensp;${message}`;
     chatBox.appendChild(chatItem);
     chatItem.scrollIntoView(true);
-
-    messValue = ' ';
+    userArray.push(chatItem);
 
     const chatItemD = document.createElement('div');
     chatItemD.classList.add('duck-chat-item');
-    chatItemD.innerHTML = `<img src="./duck.jpg" alt="duckIcon" id="duckIcon"/>&ensp;&ensp;`
+    chatItemD.innerHTML = `<img src="./duck.jpg" alt="duckIcon" id="duckIcon"/>&ensp;&ensp;`;
     chatBox.appendChild(chatItemD);
+    quackArray.push(chatItemD);
 
-    if (messValue === "food") { food(chatItemD) }
-    else setTimeout(reply(chatItemD), 100);
+    if (message == 'food') food(chatItemD);
+    else if (message == '') { quackArray[quackArray.length - 1].remove(); userArray[userArray.length - 1].remove(); }
+    else reply(chatItemD);
+
+    chatMess.value = '';
 }
 
 function reply(chatItem) {
-
-    const randomNumber = Math.floor(Math.random() * 10);
-    if (randomNumber <= 0) randomNumbers = 1;
+    let randomNumber = Math.floor(Math.random() * 10);
+    if (randomNumber <= 0) randomNumber = 1;
     let text = '';
     for (let i = 1; i <= randomNumber; i++) text += 'quack ';
     const texts = text.split(' ');
 
     let i = 0;
     function quack() {
-        if (i < texts.length) {
+        const tLength = texts.length;
+        if (i < tLength) {
             chatItem.innerHTML += `${texts[i]} `
             chatBox.appendChild(chatItem);
             audio.play();
             i++;
             setTimeout(quack, 200);
-        }
+        };
 
     };
     quack();
+
+    chatItem.scrollIntoView(true);
+};
+
+function food(chatItem) {
+    chatItem.innerHTML += `Vịt quay ngon`
+    chatBox.appendChild(chatItem);
+    audio.play();
+
     chatItem.scrollIntoView(true);
 }
 
-function food(chatItem) {
-    chatItem.innerHTML += `Vit quay ngon`
-    chatBox.appendChild(chatItem);
-    audio.play();
-    chatItem.scrollIntoView(true);
+let countdown = 5;
+let timer;
+function handleSubmit() {
+    if (canSubmit) {
+        canSubmit = false;
+        timer = setInterval(updateCountdown, 1000);
+        submit();
+    }
+}
+
+function updateCountdown() {
+    countdown--;
+    errorMess.textContent = `Waiting ${countdown} seconds`;
+    if (countdown === 0) {
+        clearInterval(timer);
+        canSubmit = true;
+        countdown = 5;
+        errorMess.textContent = "";
+    }
 }
